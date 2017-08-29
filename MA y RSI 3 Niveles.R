28151e29ddd954d40516b62dc2ea6f1a4621999a
Juan Pablo <jp.fernandezc11@gmail.com>
2017-08-29 12:38:41 -0500

    Agrega modelo de 3 niveles

--- MA y RSI 3 Niveles.R
+++ MA y RSI 3 Niveles.R
@@ -7,7 +7,7 @@ colnames(datos)<-c("Fecha","Hora","Apertura","Max","Min","Cierre","Volumen")
 
 #Cambiar formato de fecha para filtrar datos
 datos$Fecha<-as.Date(gsub("\\.","-",datos$Fecha))
-View(datos[which(datos$Fecha>="2017-01-01"),])
+#View(datos[which(datos$Fecha>="2017-01-01"),])
 
 ema.14<- as.data.frame(EMA(datos[,6],14))
 colnames(ema.14)<-"ema14"
@@ -16,7 +16,7 @@ colnames(ema.30)<-"ema30"
 rsi.14<- as.data.frame(RSI(datos[,6],14),maType="EMA")
 colnames(rsi.14)<-"rsi"
 macd<-as.data.frame(MACD(datos[,6],nFast=12,nSlow=26,nSig=9))
-bands<-as.data.frame(PBands(datos[,6],n=20,maType="SMA",sd=2))
+bands<-as.data.frame(PBands(datos[,6],n=20,maType="EMA",sd=2))
 wpr<-as.data.frame(WPR(datos[,6],n=14))
 colnames(wpr)<-"wpr"
 roc<-as.data.frame(ROC(datos[,6], n = 1, type = "discrete", na.pad = TRUE))
@@ -28,15 +28,15 @@ ret<- as.data.frame((seg-prim)/prim)
 colnames(ret)<-"ret"
 ret<-rbind("NA",ret)
 
-datos<-cbind(datos,ret,ema.14,ema.30,rsi.14,macd,"dir"=rep(0,length(datos[,1])),bands,wpr,roc)
+datos<-cbind(datos,ret,ema.14,ema.30,rsi.14,macd,"dir"=rep(0,length(datos[,1])),bands,wpr)
 datos<-datos[-c(1:64600),]
 datos<-na.omit(datos)
 #returns
 ret<-as.numeric(datos$ret)
 
 datos[,14]<-"Mantiene"
-datos[which(as.numeric(datos$ret)<=quantile(ret,.30)),14]<-"Baja"
-datos[which(as.numeric(datos$ret)>=quantile(ret,.70)),14]<-"Sube"
+datos[which(as.numeric(datos$ret)<=quantile(ret,.20)),14]<-"Baja"
+datos[which(as.numeric(datos$ret)>=quantile(ret,.80)),14]<-"Sube"
 summary(as.factor(datos[,14]))
 dir<- as.data.frame(datos[,14])
 dir<-as.data.frame(dir[-1,])
@@ -52,8 +52,8 @@ datos$ret<-as.numeric(datos$ret)
 #Dividir en entrenamiento y predicción
 datospred<- datos[301:999,]
 datos<-datos[1:300,]
-summary(datos)
-summary(datospred)
+#summary(datos)
+#summary(datospred)
 
 mod1<-rpart(datos$dir~.,data=datos)#,control = rpart.control(cp = 0.01))
 prp(mod1)
