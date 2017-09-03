099404dcf8dc3655fa86e0d78172c68aaf6fe4cc
Juan Pablo <jp.fernandezc11@gmail.com>
2017-09-03 09:54:59 -0500

    Se agrego ifelse

--- MA y RSI 3 Niveles.R
+++ MA y RSI 3 Niveles.R
@@ -8,25 +8,28 @@ colnames(datos)<-c("Fecha","Hora","Apertura","Max","Min","Cierre","Volumen")
 #Cambiar formato de fecha para filtrar datos
 datos$Fecha<-as.Date(gsub("\\.","-",datos$Fecha))
 #View(datos[which(datos$Fecha>="2017-01-01"),])
-
 ema.14<- as.data.frame(EMA(datos[,6],14))
 colnames(ema.14)<-"ema14"
 ema.30<- as.data.frame(EMA(datos[,6],30))
 colnames(ema.30)<-"ema30"
-rsi.14<- as.data.frame(RSI(datos[,6],14),maType="EMA")
+rsi.14<- as.data.frame(RSI(datos[,6],14,maType='EMA'))
 colnames(rsi.14)<-"rsi"
 macd<-as.data.frame(MACD(datos[,6],nFast=12,nSlow=26,nSig=9))
 bands<-as.data.frame(PBands(datos[,6],n=20,maType="EMA",sd=2))
 wpr<-as.data.frame(WPR(datos[,6],n=14))
 colnames(wpr)<-"wpr"
-roc<-as.data.frame(ROC(datos[,6], n = 1, type = "discrete", na.pad = TRUE))
-colnames(roc)<-"roc"
 ret<-datos[,6]-lag(datos[,6],k=1)
 seg<-datos[-1,6]
 prim<-datos[-length(datos[,1]),6]
 ret<- as.data.frame((seg-prim)/prim)
 colnames(ret)<-"ret"
 ret<-rbind("NA",ret)
+
+##### Prueba ifelse
+datos1<-na.omit(cbind(datos,ret,ema.14,ema.30,rsi.14,macd))
+ret<-as.numeric(datos1$ret)
+datos1$dir<-ifelse(as.numeric(datos$ret)<=quantile(ret,.20),"Baja",ifelse(as.numeric(datos$ret)>=quantile(ret,.80),"Sube","Mantiene"))
+#####
 
 datos<-cbind(datos,ret,ema.14,ema.30,rsi.14,macd,"dir"=rep(0,length(datos[,1])),bands,wpr)
 datos<-datos[-c(1:64600),]
@@ -50,10 +53,12 @@ datos<-na.omit(datos)
 datos<-datos[,-c(1:5)]
 datos$ret<-as.numeric(datos$ret)
 #Dividir en entrenamiento y predicción
-datospred<- datos[301:999,]
+datospred<- datos[301:399,]
 datos<-datos[1:300,]
 #summary(datos)
 #summary(datospred)
+x<-sapply(datos, function(x) typeof(x))
+View(x)
 
 mod1<-rpart(datos$dir~.,data=datos)#,control = rpart.control(cp = 0.01))
 prp(mod1)
@@ -70,4 +75,4 @@ table(pred1)
 table(pred1, datospred$dir)
 conf<-table(pred1, datospred$dir)
 sum(diag(conf))/sum(conf)
-
+View(as.data.frame(cbind(pred1,datospred$ret)))
